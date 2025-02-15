a4f6f83e228532269b327241b70ccad470f8dab7
MO <xmike.mox@gmail.com>
2025-02-15 19:59:20 -0300

    Insert students into the system

--- treino/api.py
+++ treino/api.py
@@ -1,7 +1,30 @@
 from ninja import Router
+from .schemas import AlunosSchema
+from .models import Alunos
+from ninja.errors import HttpError
+from typing import List
 
 treino_router = Router()
 
-@treino_router.get('')
-def criar_aluno(request):
-    return {'Ola mundo!': 'Ola mundo!'}
+@treino_router.post('', response={200: AlunosSchema})
+def criar_aluno(request, aluno_schema: AlunosSchema):
+    nome = aluno_schema.dict()['nome']
+    email = aluno_schema.dict()['email']
+    faixa = aluno_schema.dict()['faixa']
+    data_nascimento = aluno_schema.dict()['data_nascimento']
+
+    if Alunos.objects.filter(email=email).exists():
+        raise HttpError(400, "E-mail já cadastrado.")
+
+    aluno = Alunos(nome=nome,
+                    email=email, 
+                    faixa=faixa,
+                    data_nascimento=data_nascimento)
+    aluno.save()
+
+    return aluno
+
+@treino_router.get('/alunos/', response=List[AlunosSchema])
+def listar_alunos(request):
+    alunos = Alunos.objects.all()
+    return alunos
